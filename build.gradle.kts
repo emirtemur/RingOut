9c577c73c9909a0024ce7a628ae4991e01386820
Emir Temur <emirtmr1@gmail.com>
2026-09-25 20:15:33 +0300

    chore: set up gradle build for paper 1.21.4 plugin

---
+++ build.gradle.kts
@@ -0,0 +1,41 @@
+plugins {
+    java
+    id("xyz.jpenilla.run-paper") version "3.1.0"
+}
+
+group = "me.emirtemur"
+version = "1.0.0"
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
+}
+
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:1.21.4-R0.1-SNAPSHOT")
+}
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(21))
+}
+
+tasks {
+    compileJava {
+        options.encoding = "UTF-8"
+        options.release.set(21)
+    }
+
+    processResources {
+        val props = mapOf("version" to project.version)
+        inputs.properties(props)
+        filteringCharset = "UTF-8"
+        filesMatching("plugin.yml") {
+            expand(props)
+        }
+    }
+
+    runServer {
+        minecraftVersion("1.21.4")
+    }
+}
